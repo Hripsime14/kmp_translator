f773027bcc20e4ef2621bd8c970af908a6020272
Hripsime <hripsime.ghazaryan98@gmail.com>
2024-08-02 14:49:07 +0400

    Add voice to text fab.

--- androidApp/src/main/java/com/example/kmp_translator/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/kmp_translator/android/MainActivity.kt
@@ -11,13 +11,16 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.kmp_translator.Greeting
 import com.example.kmp_translator.android.core.presentation.Routes
 import com.example.kmp_translator.android.translate.presentation.AndroidTranslateViewModel
 import com.example.kmp_translator.android.translate.presentation.TranslateScreen
+import com.example.kmp_translator.translate.presentation.TranslateEvent
 import dagger.hilt.android.AndroidEntryPoint
 import okhttp3.Route
 
@@ -46,13 +49,31 @@ fun TranslateRoot(
     NavHost(
         navController = navController,
         startDestination = Routes.TRANSLATE ) {
-        composable(route = Routes.TRANSLATE) {
-            val viewModel = hiltViewModel<AndroidTranslateViewModel>()
-            val state by viewModel.state.collectAsState()
-            TranslateScreen(
-                state = state,
-                onEvent = viewModel::onEvent
-            )
-        }
+            composable(route = Routes.TRANSLATE) {
+                val viewModel = hiltViewModel<AndroidTranslateViewModel>()
+                val state by viewModel.state.collectAsState()
+                TranslateScreen(
+                    state = state,
+                    onEvent = { event ->
+                        when (event) {
+                            is TranslateEvent.RecordAudio -> {
+                                navController.navigate(Routes.VOICE_TO_TEXT + "/${state.fromLanguage.language.langCode}")
+                            }
+                            else -> viewModel.onEvent(event)
+                        }
+                    }
+                )
+            }
+            composable(
+                route = Routes.VOICE_TO_TEXT + "/{languageCode}",
+                arguments = listOf(
+                    navArgument("languageCode") {
+                        type = NavType.StringType
+                        defaultValue = "en"
+                    }
+                )
+            ) {
+                Text(text = "Voice-To-Text")
+            }
     }
 }
